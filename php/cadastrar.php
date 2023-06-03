ea0de4d14133f6caa58b6ab56429f16c82a1f090
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-06-02 22:20:31 -0300

    feat: cadastrando cursos

---
+++ php/cadastrar.php
@@ -0,0 +1,22 @@
+<?php  
+
+include("conexao.php");
+
+$obterDados = file_get_contents("php://input");
+
+$extrair = json_decode($obterDados);
+
+$nomeCurso = $extrair->cursos->nomeCurso;
+$valorCurso = $extrair->cursos->valorCurso;
+
+$sql = "insert into cursos (nomeCurso, valorCurso) values ('$nomeCurso', valorCurso)";
+mysqli_query($conexao, $sql);
+
+$curso = [
+    'nomeCurso' => $nomeCurso,
+    'valorCurso' => $valorCurso
+];
+
+json_encode(['curso'=>$curso]);
+
+?>
